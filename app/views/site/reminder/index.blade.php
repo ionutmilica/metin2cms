c85ad7d1ec8559c4c16df885d1ca44b3c53380af
Vlad Marian Gabriel <blackburn1911@gmail.com>
2014-09-23 22:54:46 +0300

    Reset password errors

--- app/views/site/reminder/index.blade.php
+++ app/views/site/reminder/index.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.default')
 
 @section('content')
-@include('layouts.partials.alerts')
 <div class="panel panel-default">
     <div class="panel-heading">
         Recover your password
     </div>
     <div class="panel-body">
         <form role="form" action="{{ route('account.recover') }}" method="post">
-            <div class="form-group">
+            <div class="form-group @if ($errors->has('username')) has-error @endif">
                 <label for="username">Username</label>
                 <input type="username" class="form-control" name="username" id="username" placeholder="Enter username">
+                @if ($errors->has('username'))
+                    <span class="help-block">{{ $errors->first('username') }} </span>
+                @endif
             </div>
-            <div class="form-group">
+            <div class="form-group @if ($errors->has('email')) has-error @endif">
                 <label for="email">Email</label>
                 <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
+                @if ($errors->has('email'))
+                    <span class="help-block">{{ $errors->first('email') }} </span>
+                @endif
             </div>
             <button type="submit" class="btn btn-primary">Recover password</button>
         </form>
