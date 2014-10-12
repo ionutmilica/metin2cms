123b417b66858b92931cd30e5e21ac70c13dff08
ionutxp <ionut.milica@gmail.com>
2014-10-12 17:43:45 +0300

    Fixing typo

--- app/config/register.php
+++ app/config/register.php
@@ -2,6 +2,6 @@
 
 return array(
     'closed' => false,
-    'message' => 'Registration is colosed for now due to high number of accounts.',
+    'message' => 'Registration is closed for now due to high number of accounts.',
     'confirmation' => false,
 );
