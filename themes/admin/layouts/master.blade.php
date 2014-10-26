3830a761af060d6abbb96e92abc3d90657fa8993
ionutxp <ionut.milica@gmail.com>
2014-10-26 20:12:07 +0200

    No need for breadcumb

--- themes/admin/layouts/master.blade.php
+++ themes/admin/layouts/master.blade.php
@@ -91,16 +91,11 @@
                         Dashboard
                         <small>Home</small>
                     </h1>
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                        <li class="active">Dashboard</li>
-                    </ol>
                 </section>
 
                 <!-- Main content -->
                 <section class="content">
-                    <div class="row">
-                    </div><!-- /.row (main row) -->
+                   
                 </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
