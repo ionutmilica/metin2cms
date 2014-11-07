196946a11018be5e305868083cd2166fe7e73fae
ionutxp <ionut.milica@gmail.com>
2014-11-07 18:57:29 +0200

    Cleaning the sidebar a little bit

--- themes/admin/layouts/partials/sidebar.blade.php
+++ themes/admin/layouts/partials/sidebar.blade.php
@@ -1,42 +1,42 @@
 <section class="sidebar">
-    <a href="{{ route('home') }}" class="btn btn-primary btn-lg" style="margin: 10px 0 10px 50px; color: #ffffff;" target="_blank">View Site</a>
     <ul class="sidebar-menu">
-        <li {{ Route::currentRouteName() == 'admin.home' ? 'class="active"' : '' }}>
+        <a href="{{ route('home') }}" class="btn btn-primary btn-lg" style="margin: 10px 0 10px 50px; color: #ffffff;" target="_blank">View Site</a>
+        <li {{ Route::is('admin.home') ? 'class="active"' : '' }}>
             <a href="{{ route('admin.home') }}">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
             </a>
         </li>
-        <li {{ Route::currentRouteName() == 'admin.account.index' ? 'class="active"' : '' }}>
+        <li {{ Route::is('admin.account.index') ? 'class="active"' : '' }}>
             <a href="{{ route('admin.account.index') }}">
                 <i class="fa fa-dashboard"></i> <span>Accounts</span>
             </a>
         </li>
         <!-- Players -->
-         <li class="treeview {{ Route::currentRouteName() == 'admin.player.index' ? 'active' : '' }}">
+         <li class="treeview {{ Route::is('admin.player.index') ? 'active' : '' }}">
             <a href="#">
                 <i class="fa fa-table"></i> <span>Players</span>
                 <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="treeview-menu">
-                <li {{ Route::currentRouteName() == 'admin.player.index' ? 'class="active"' : '' }}>
+                <li {{ Route::is('admin.player.index') ? 'class="active"' : '' }}>
                     <a href="{{ route('admin.player.index')  }}">
                         <i class="fa fa-angle-double-right"></i> Players list
                     </a>
                 </li>
             </ul>
         </li>
-         <li class="treeview {{ Route::currentRouteName() == 'admin.staff.index' || Route::currentRouteName() == 'admin.staff.create' ? 'active' : '' }}">
+         <li class="treeview {{ Route::is('admin.staff.index') || Route::is('admin.staff.create') ? 'active' : '' }}">
             <a href="#">
                 <i class="fa fa-table"></i> <span>Ingame staff</span>
                 <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="treeview-menu">
-                <li {{ Route::currentRouteName() == 'admin.staff.index' ? 'class="active"' : '' }}>
+                <li {{ Route::is('admin.staff.index') ? 'class="active"' : '' }}>
                     <a href="{{ route('admin.staff.index')  }}">
                         <i class="fa fa-angle-double-right"></i> Staff list
                     </a>
                 </li>
-                <li {{ Route::currentRouteName() == 'admin.staff.create' ? 'class="active"' : '' }}>
+                <li {{ Route::is('admin.staff.create') ? 'class="active"' : '' }}>
                     <a href="{{ route('admin.staff.create')  }}">
                         <i class="fa fa-angle-double-right"></i> Add to staff
                     </a>
